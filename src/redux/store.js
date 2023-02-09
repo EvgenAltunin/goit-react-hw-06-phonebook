8f6fc30903d3cf7fa4ebe9c67a815a4c6c4de7ff
Evgen Altunin <106544836+EvgenAltunin@users.noreply.github.com>
2023-02-10 01:22:56 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducer';
 
 export const store = configureStore({
